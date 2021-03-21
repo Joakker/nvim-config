4a7d9841b2343f4130bbc6ed3f6fc4b4d5c50d59
Joakker <joaquinandresleon108@gmail.com>
2021-03-21 10:24:24 -0300

    Add more mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -12,10 +12,11 @@ for _, dir in ipairs{'h','j','k','l'} do
     set_keymap('n', string.format('<M-%s', dir), string.format('<C-w>%s', dir))
 end
 
+set_keymap('n', '<F1>', '<CMD>Telescope help_tags<CR>')
 set_keymap('n', '<F2>', ':tabnew ', {silent = false})
 
 set_keymap('n', '<leader>vs', '<CMD>lua require"utils".reload_config()<CR>')
--- set_keymap('n'
--- set_keymap('n'
--- set_keymap('n'
+set_keymap('n', '<leader>wo', '<CMD>only<CR>')
+set_keymap('n', '<leader>wc', '<CMD>close<CR>')
+set_keymap('n', '<leader>ms', '<CMD>messages<CR>')
 -- set_keymap('n'
