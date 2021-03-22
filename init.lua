83f4012b2514f7677e75ac06be60e48c5f083524
Joakker <joaquinandresleon108@gmail.com>
2021-03-22 09:15:12 -0300

    Document import

--- init.lua
+++ init.lua
@@ -1,8 +1,12 @@
+-- Wrapper around require that reloads the given package every time
+-- it is called.
+--
+---@param   path    string      The package's path. Use require syntax
+---@return  any
 function _G.import(path)
     package.loaded[path] = nil
     return require(path)
 end
-
 
 import 'config'
 import 'plugins'
