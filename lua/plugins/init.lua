c2d93ac0adc96dc043c3b17f0294f5706617aa6f
Joakker <joaquinandresleon108@gmail.com>
2021-03-22 10:56:43 -0300

    Add gitsigns.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,4 +50,11 @@ return require'packer'.startup(function()
         'nvim-telescope/telescope.nvim',
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
     }
+    use {
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require'gitsigns'.setup()
+        end,
+        requires = 'nvim-lua/plenary.nvim'
+    }
 end)
