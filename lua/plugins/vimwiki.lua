e938f0745596167fdbb9625cbba70397020884a1
Joakker <joaquinandresleon108@gmail.com>
2021-03-29 01:45:50 -0300

    Don't use snippets in vimwiki files. For now at least

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -2,13 +2,13 @@ local g = vim.g
 
 g.vimwiki_hl_headers = 1
 g.vimwiki_hl_cb_checked = 2
-g.vimwiki_key_mappings = {
+--[[ g.vimwiki_key_mappings = {
     global = false,
     text_objs = false,
     table_mappings = true,
     links = false,
     html = false
-}
+} ]]
 g.vimwiki_list = {
     {
         path = '~/Documents/Wiki',
