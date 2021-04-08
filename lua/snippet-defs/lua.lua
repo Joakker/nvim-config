f7dfcfd9b9ecdb70986d2d16e88a5cbaa532d9ec
Joakker <joaquinandresleon108@gmail.com>
2021-04-07 21:19:09 -0400

    Define snippets for function and lambda

--- lua/snippet-defs/lua.lua
+++ lua/snippet-defs/lua.lua
@@ -3,7 +3,10 @@ local i = u.match_indentation
 
 return {
     req = [[local ${2:${1|S.v:match"([^.()]+)[()]*$"}} = require '$1']],
-    func = [[function${1|vim.trim(S.v):gsub("^%S", " %0")}(${2|vim.trim(S.v)})$0 end]],
+    func = [[function${1|vim.trim(S.v):gsub("^%S", " %0")}(${2|vim.trim(S.v)})
+    $0
+end]],
+    lambda = [[function(${2|vim.trim(S.v)}) $0 end]],
     ['for'] = i [[
 for ${1:k}, ${2:v} in $4 do
     $0
