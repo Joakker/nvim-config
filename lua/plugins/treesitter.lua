12ce46a2a026a9fc362a2f6418a833313deabdf8
Joakker <joaquinandresleon108@gmail.com>
2021-04-14 09:03:19 -0400

    Enable indent module in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 require'nvim-treesitter.configs'.setup {
     ensure_installed = 'maintained',
     highlight = {enable = true},
-    indent = {enable = true},
+    indent = {enable = true, disable = {'python'}},
     textobjects = {
         select = {
             enable = true,
