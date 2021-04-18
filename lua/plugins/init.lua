a607d8b2a55fdda59c858ac476e3ba1d06eaf1b0
Joakker <joaquinandresleon108@gmail.com>
2021-04-17 21:45:15 -0400

    Add hop.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,29 +1,9 @@
-local fn   = vim.fn
-
-local install_path = fn.stdpath('data') .. 'site/pack/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system{
-        'git', 'clone',
-        'https://github.com/wbthomason/packer.nvim',
-        install_path
-    }
-end
-
 local packer = require 'packer'
 
 packer.init {
-    git = {
-        clone_timeout = 3600
-    },
-    display = {
-        working_sym = '',
-        error_sym   = '',
-        done_sym    = '',
-    },
-    luarocks = {
-        python_cmd = 'python3'
-    }
+    git = {clone_timeout = 3600},
+    display = {working_sym = '', error_sym = '', done_sym = ''},
+    luarocks = {python_cmd = 'python3'}
 }
 
 local use = packer.use
@@ -47,43 +27,39 @@ packer.startup(function()
     use {
         'neovim/nvim-lspconfig',
         requires = {
-            'nvim-lua/completion-nvim',
-            'SirVer/ultisnips',
-            'honza/vim-snippets'
-        },
+            'nvim-lua/completion-nvim', 'SirVer/ultisnips', 'honza/vim-snippets'
+        }
     }
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
-        requires = {
-            'p00f/nvim-ts-rainbow'
-        },
+        requires = {'p00f/nvim-ts-rainbow'},
         config = function() require 'plugins.treesitter' end
     }
     use {
         'crispgm/nvim-go',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-lua/popup.nvim'
-        },
+        requires = {'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim'},
         config = function() require 'plugins.go' end
     }
     use {
         'nvim-telescope/telescope.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-lua/popup.nvim'
-        },
+        requires = {'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim'},
         config = function() require 'plugins.telescope' end
     }
     use {
         'numtostr/FTerm.nvim',
+        as = 'fterm',
         config = function() require 'plugins.fterm' end
     }
     use {
         'kyazdani42/nvim-tree.lua',
         requires = 'kyazdani42/nvim-web-devicons',
         config = function() require 'plugins.nvim-tree' end
+    }
+    use {
+        'phaazon/hop.nvim',
+        as = 'hop',
+        config = function() require 'plugins.hop' end
     }
 
     -- Simple Plugins
