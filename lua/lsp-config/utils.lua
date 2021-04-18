163bd03ff55e9bbd27080165b499c3de6abaf391
Joakker <joaquinandresleon108@gmail.com>
2021-04-17 21:46:38 -0400

    Use formatting on save if available

--- lua/lsp-config/utils.lua
+++ lua/lsp-config/utils.lua
@@ -17,6 +17,12 @@ local function on_attach(client, bufnr)
     end
     if client.resolved_capabilities.document_formatting then
         set_keymap('n', '<leader>lf', '<CMD>lua vim.lsp.buf.formatting()<CR>')
+        vim.api.nvim_exec([[
+        augroup lsp_formatting
+            autocmd! * <buffer>
+            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+        augroup END
+        ]], false)
     end
 end
 
