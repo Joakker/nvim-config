9e817f63fc3b0b717accd4601b4bcbe676c14c8e
Joakker <joaquinandresleon108@gmail.com>
2021-04-29 12:20:26 -0400

    Highlight stuff with Aurora colours

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,13 @@
-vim.cmd [[hi GitSignsChange guifg=#FF0000]]
+vim.cmd [[hi GitSignsChange guifg=#BF616A]]
+vim.cmd [[hi GitSignsAdd    guifg=#4C566A]]
+vim.cmd [[hi GitSignsChange guifg=#EBCB8B]]
 
-require'gitsigns'.setup {}
+require'gitsigns'.setup {
+    signs = {
+        add = {text = '┃'},
+        change = {text = '┃'},
+        delete = {text = '┃'},
+        topdelete = {text = '┏'},
+        changedelete = {text = '┣'},
+    },
+}
