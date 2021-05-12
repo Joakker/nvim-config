028a440c3a2da11a07d0d10fca2dc656f91dd180
Joakker <joaquinandresleon108@gmail.com>
2021-05-11 22:41:15 -0400

    Disable formatting for clangd

--- lua/lsp-config/clangd.lua
+++ lua/lsp-config/clangd.lua
@@ -2,5 +2,8 @@ local utils = require 'lsp-config.utils'
 
 require'lspconfig'.clangd.setup {
     cmd = {'clangd-11', '--background-index'},
-    on_attach = utils.on_attach
+    on_attach = function(client, bufnr)
+        client.resolved_capabilities.document_formatting = false
+        utils.on_attach(client, bufnr)
+    end,
 }
