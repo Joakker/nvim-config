079cc81e8f41a9c4b298615758a4e48faeac2bd8
Joakker <joaquinandresleon108@gmail.com>
2021-05-22 21:26:55 -0400

    Configure bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,1 +1,13 @@
-require'bufferline'.setup {}
+require'bufferline'.setup {
+    options = {
+        separator_style = 'slant',
+        show_buffer_close_icons = false,
+        diagnostics = 'nvim_lsp',
+        diagnostics_indicator = function(count)
+            return ' (' .. count .. ')'
+        end,
+        custom_filter = function(bufnr)
+            if vim.bo[bufnr].filetype ~= 'qf' then return true end
+        end,
+    },
+}
