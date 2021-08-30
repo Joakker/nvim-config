70b8b130fc1ee4a163cbb094764a678a775dff75
Joaquín León <joaquinandresleon108@gmail.com>
2021-08-30 18:36:03 -0400

    Neorg's tree-sitter parser is broken

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -16,14 +16,6 @@ parser_configs.kotlin = {
     filetype = 'kotlin',
 }
 
-parser_configs.norg = {
-    install_info = {
-        url = 'https://github.com/vhyrro/tree-sitter-norg',
-        files = { 'src/parser.c' },
-        branch = 'main',
-    },
-}
-
 require('nvim-treesitter.configs').setup {
     ensure_installed = 'maintained',
     highlight = {
