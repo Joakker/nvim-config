19e57e9cd86776c1a46799f1ae45ec587811792d
Joaquín León <joaquinandresleon108@gmail.com>
2021-09-05 23:00:26 -0300

    Add some neat autopairs

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -5,6 +5,36 @@ local cond = require 'nvim-autopairs.conds'
 npairs.setup {}
 
 npairs.add_rules {
+    -- Globals
+    Rule(' ', ' '):with_pair(function(opts)
+        local pair = opts.line:sub(opts.col - 1, opts.col)
+        return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+    Rule('( ', ' )')
+        :with_pair(function()
+            return false
+        end)
+        :with_move(function(opts)
+            return opts.prev_char:match '.%)' ~= nil
+        end)
+        :use_key ')',
+    Rule('{ ', ' }')
+        :with_pair(function()
+            return false
+        end)
+        :with_move(function(opts)
+            return opts.prev_char:match '.%}' ~= nil
+        end)
+        :use_key '}',
+    Rule('[ ', ' ]')
+        :with_pair(function()
+            return false
+        end)
+        :with_move(function(opts)
+            return opts.prev_char:match '.%]' ~= nil
+        end)
+        :use_key ']',
+
     -- Python
     Rule("'''", "'''", { 'python' }),
     Rule('"""', '"""', { 'python' }),
@@ -12,4 +42,9 @@ npairs.add_rules {
     -- Latex
     Rule('$', '$', { 'latex' }):with_move(cond.none()),
     Rule('$$', '$$', { 'latex' }),
+
+    -- ECMAScript
+    Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'javascript' })
+        :use_regex(true)
+        :set_end_pair_length(2),
 }
