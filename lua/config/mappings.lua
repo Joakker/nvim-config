5c4e6deedba78e197496a2d37b90d2f9064c4a75
Joaquín León <joaquinandresleon108@gmail.com>
2021-09-09 13:48:36 -0300

    Add mappings for window hopping

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,16 +3,24 @@ local bufferline = require 'bufferline'
 
 vim.g.mapleader = ','
 
+-- Window/Buffer navigation
 k.nnoremap { '<C-k>', partial(bufferline.cycle, -1) }
 k.nnoremap { '<C-j>', partial(bufferline.cycle, 1) }
+k.nnoremap { '<Left>', '<C-w>h' }
+k.nnoremap { '<Right>', '<C-w>l' }
+k.nnoremap { '<Up>', '<C-w>k' }
+k.nnoremap { '<Down>', '<C-w>j' }
 
+-- Packer specific mappings
 k.nnoremap { '<leader>pi', require('packer').install }
 k.nnoremap { '<leader>pu', require('packer').update }
 k.nnoremap { '<leader>pc', require('packer').compile }
 
+-- Bouncy bouncy
 k.nnoremap { '<leader><leader>w', require('hop').hint_words }
 k.nnoremap { '<leader><leader>l', require('hop').hint_lines }
 
+-- Function keys
 k.nnoremap { '<F1>', require('telescope.builtin').help_tags }
 k.nnoremap { '<F2>', ':edit<SPACE>', silent = false }
 k.nnoremap { '<F3>', require('nvim-tree').toggle }
